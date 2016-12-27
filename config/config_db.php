3412f12f69c1ca70c5d532e17e81498b305c74e6
7142-felipe <tecnologiagrave@gmail.com>
2016-12-27 19:29:02 -0200

    Arquivos do GLPI v9.1

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+ class DB extends DBmysql {
+                
+ public $dbhost     = 'localhost';
+                
+ public $dbuser     = 'glpi2';
+                
+ public $dbpassword = 'glpi2';
+                
+ public $dbdefault  = 'glpi';
+                
+}
